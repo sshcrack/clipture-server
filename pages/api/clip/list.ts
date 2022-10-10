508d24a279a8880ff85272c88ef9d21d9f37cb94
sshcrack <hendrik.lind777@gmail.com>
2022-10-10 14:12:04 +0200

    added more info to list

--- pages/api/clip/list.ts
+++ pages/api/clip/list.ts
@@ -4,23 +4,26 @@ import { checkBanned, prisma } from "../../../util/db";
 
 export default async function ListClips(req: NextApiRequest, res: NextApiResponse) {
     const user = await getServerUser(req)
-    if(!user)
-        return res.status(403).json({ error: "Unauthenticated."})
+    if (!user)
+        return res.status(403).json({ error: "Unauthenticated." })
 
-    if(await checkBanned(user.id, res))
+    if (await checkBanned(user.id, res))
         return
 
 
     const clips = await prisma.clip.findMany({
         where: {
             uploaderId: user.id
-        }
+        },
+        include: { windowInfo: true }
     })
 
-    const filteredInfo = clips.map(({ id, uploadDate, title}) => ({
+    const filteredInfo = clips.map(({ id, uploadDate, title, dcGameId, windowInfo }) => ({
         id,
         uploadDate,
-        title
+        title,
+        dcGameId,
+        windowInfo
     }))
 
     res.json(filteredInfo)

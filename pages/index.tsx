872975a0b9b536111596941f891c2c27f8a9bc8b
sshcrack <hendrik.lind777@gmail.com>
2022-10-28 20:59:59 +0200

    added meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex } from '@chakra-ui/react'
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import Footer from '../components/Home/Footer'
 import LogoMain from '../components/Home/LogoMain'
 import Sections from '../components/Sections'
@@ -9,6 +10,17 @@ const Home: NextPage = () => {
     w='100%'
     h='100%'
   >
+    <Head>
+      <title>Clipture</title>
+      <meta name="keywords" content="Clipture, Clip, Platform, Cloud, Cut, Electron, Typescript, Game" />
+      <meta name="description" content="Record your favorite games!" />
+      <meta name="author" content="sshcrack" />
+      <meta name="og:type" content="website" />
+      <meta name="og:url" content="https://clipture.sshcrack.me" />
+      <meta name="og:site_name" content="Clipture" />
+      <meta name="og:image" content="/img/cover/cover.png" />
+      <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+    </Head>
     <Flex
       flexDir='column'
       w='100%'

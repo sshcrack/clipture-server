c251b1be64a8ceb0426a1681d7be2ef406d20a5f
sshcrack <hendrik.lind777@gmail.com>
2022-10-30 19:48:38 +0100

    cooler download button

--- components/Home/DownloadButtons.tsx
+++ components/Home/DownloadButtons.tsx
@@ -6,21 +6,23 @@ import styles from "../../styles/General/DownloadButton.module.scss"
 export default function DownloadButtons() {
     return <Link href='/api/download'>
         <a>
-            <Flex
-                className={styles.buttonOuter}
-            >
-                <Text
-                    className={styles.buttonText}
-                >Download</Text>
+            <Flex className={styles.glowBox}>
                 <Flex
-                    className={styles.buttonDownload}
+                    className={styles.buttonOuter}
                 >
-                    <BsArrowDown style={{
-                        width: "100%",
-                        height: "100%",
-                        minHeight: "1.5em",
-                        minWidth: "1.5em"
-                    }} />
+                    <Text
+                        className={styles.buttonText}
+                    >Download</Text>
+                    <Flex
+                        className={styles.buttonDownload}
+                    >
+                        <BsArrowDown style={{
+                            width: "100%",
+                            height: "100%",
+                            minHeight: "1.5em",
+                            minWidth: "1.5em"
+                        }} />
+                    </Flex>
                 </Flex>
             </Flex>
         </a>

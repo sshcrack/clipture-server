29cbfa97287945c6d808672694c41eeb3330ba88
sshcrack <hendrik.lind777@gmail.com>
2022-11-14 18:37:34 +0100

    fixed download assets file

--- scripts/downloadAssets.js
+++ scripts/downloadAssets.js
@@ -6,47 +6,55 @@ const path = require("path")
 const { prettyBytes } = require("./prettyBytes")
 const { promisify } = require("node:util")
 
+const currHash = "58b0f7a94ee51e587cd5ae82be15c456796c67d6469c7872f165e69976141adf"
+
 const pipeline = promisify(stream.pipeline);
 const fct = async () => {
-    console.log("Downloading assets from dropbox...")
-    const downloadLink = "https://www.dropbox.com/s/e0ziubp60mxx3p1/assets.zip?dl=1"
-    const req = got.stream(downloadLink)
-
-    req.on("downloadProgress", e => {
-        console.log(`Downloading ${Math.round(e.percent * 100 * 100) / 100}% (${isNaN(e.transferred) ? "NaN" : prettyBytes(e.transferred)}/${isNaN(e.total) ? "NaN" : prettyBytes(e.total)})`)
-    })
-
-    await pipeline(
-        req,
-        fs.createWriteStream('assets.zip')
-    );
-
+    const dotFile = ".downloaded-assets-hash"
+    if (fs.existsSync(dotFile)) {
+        const hash = fs.readFileSync(dotFile, "utf-8")
+        if (hash === currHash) {
+            console.log(hash, "and", currHash, "are equal, not downloading new assets.")
+            return
+        }
+    }
+    
+        console.log("Downloading assets from dropbox...")
+        const downloadLink = "https://www.dropbox.com/s/e0ziubp60mxx3p1/assets.zip?dl=1"
+        const req = got.stream(downloadLink)
+    
+        req.on("downloadProgress", e => {
+            console.log(`Downloading ${Math.round(e.percent * 100 * 100) / 100}% (${isNaN(e.transferred) ? "NaN" : prettyBytes(e.transferred)}/${isNaN(e.total) ? "NaN" : prettyBytes(e.total)})`)
+        })
+    
+        await pipeline(
+            req,
+            fs.createWriteStream('assets.zip')
+        );
+    
     console.log("Unzipping...")
     const fileContent = fs.readFileSync("assets.zip")
-    JSZip.loadAsync(fileContent, function (err, data) {
-        if (err) {
-            console.log("Error, deleting archive...")
-            throw err; // or handle err
+    JSZip.loadAsync(fileContent).then(async data => {
+
+        console.log("Zip loaded...")
+        const keys = Object.keys(data.files)
+        for (let key of keys) {
+            const item = data.files[key]
+            if (item.dir) {
+                fs.mkdirSync(path.join("public", item.name))
+            } else {
+                console.log("Unpacking", item.name)
+                fs.writeFileSync(path.join("public", item.name), Buffer.from(await item.async("arraybuffer")))
+            }
         }
 
-        JSZip.loadAsync(data).then(async zip => {
-            console.log("Zip loaded...")
-            const keys = Object.keys(zip.files)
-            for (let key of keys) {
-                const item = result.files[key]
-                if (item.dir) {
-                    fs.mkdirSync(path.join("public", item.name))
-                } else {
-                    console.log("Unpacking", item.name)
-                    fs.writeFileSync(path.join("public", item.name), Buffer.from(await item.async("arraybuffer")))
-                }
-            }
-        })
-            .catch(err => {
-                console.log("Error, deleting archive...")
-                throw err; // or handle err
-            });
-    });
+        fs.writeFileSync(dotFile, currHash)
+        console.log("Done.")
+    })
+        .catch(err => {
+            console.log("Error, deleting archive...")
+            throw err; // or handle err
+        });
 }
 
 fct().finally(() => fs.existsSync("assets.zip") && fs.unlinkSync("assets.zip"))

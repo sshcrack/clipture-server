4a7ebe90fc3b3d1c1796ee8446d76f87ab60596c
sshcrack <hendrik.lind777@gmail.com>
2022-11-15 22:33:00 +0100

    added more download options

--- components/Home/DownloadButtons.tsx
+++ components/Home/DownloadButtons.tsx
@@ -4,27 +4,34 @@ import { BsArrowDown } from "react-icons/bs";
 import styles from "../../styles/General/DownloadButton.module.scss"
 
 export default function DownloadButtons() {
-    return <Link href='/api/download'>
-        <a>
-            <Flex className={styles.glowBox}>
-                <Flex
-                    className={styles.buttonOuter}
-                >
-                    <Text
-                        className={styles.buttonText}
-                    >Download</Text>
+    return <>
+        <Link href='/api/download'>
+            <a>
+                <Flex className={styles.glowBox}>
                     <Flex
-                        className={styles.buttonDownload}
+                        className={styles.buttonOuter}
                     >
-                        <BsArrowDown style={{
-                            width: "100%",
-                            height: "100%",
-                            minHeight: "1.5em",
-                            minWidth: "1.5em"
-                        }} />
+                        <Text
+                            className={styles.buttonText}
+                        >Download</Text>
+                        <Flex
+                            className={styles.buttonDownload}
+                        >
+                            <BsArrowDown style={{
+                                width: "100%",
+                                height: "100%",
+                                minHeight: "1.5em",
+                                minWidth: "1.5em"
+                            }} />
+                        </Flex>
                     </Flex>
                 </Flex>
-            </Flex>
-        </a>
-    </Link>
+            </a>
+        </Link>
+        <Link href='https://github.com/sshcrack/clipture/releases/latest'>
+            <a style={{paddingTop: "var(--chakra-space-8)"}}>
+                <Text>More download options</Text>
+            </a>
+        </Link>
+    </>
 }
